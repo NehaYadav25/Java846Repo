d9d33fa5aab7d439a2f80baf74412b281e122b89
NehaYadav25 <48085744+NehaYadav25@users.noreply.github.com>
2020-08-05 15:04:11 +0530

    product function added

--- SampleProject.java
+++ SampleProject.java
@@ -2,7 +2,10 @@ class Calculator{
    public void add(int a, int b){
       System.out.println("Sum="+a+b);
     }
-   public vloid diff(int a,int b){
+   public void diff(int a,int b){
       System.out.println("Diff"+ (a-b));
+   }
+   public void prod(int a, int b){
+       System.out.println("Product= "+(a*b));
    }
 }
