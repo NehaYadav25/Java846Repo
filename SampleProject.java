31870eafe4173faaff2042a5f39b2306c2465c2f
NehaYadav25 <48085744+NehaYadav25@users.noreply.github.com>
2020-08-05 15:09:57 +0530

    div module added

--- SampleProject.java
+++ SampleProject.java
@@ -2,7 +2,10 @@ class Calculator{
    public void add(int a, int b){
       System.out.println("Sum="+a+b);
     }
-   public vloid diff(int a,int b){
+   public void diff(int a,int b){
       System.out.println("Diff"+ (a-b));
+   }
+   public void div(int a,int b){
+      System.out.println("Quotient"+ (a/b));
    }
 }
